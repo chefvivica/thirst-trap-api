d2f3bad94d8129301f697b73735e12d3b73e4554
Leon Jamison <leonjamison@icloud.com>
2020-05-08 13:41:55 -0400

    something

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -3,7 +3,8 @@ class DrinksController < ApplicationController
     def index
         drinks = Drink.all
 
-        render json:drinks
+        # render json:drinks
+        render json:drinks.to_json(include: [:reviews, :users])
     end
 
     def show
